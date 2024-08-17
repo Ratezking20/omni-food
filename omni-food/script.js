8cb7d851db20927b7943b536e149dac154a9d3fb
Ratezking20 <ratez1214@gmail.com>
2024-08-17 14:29:36 -0700

    Omnifood website 8/17/2024

---
+++ omni-food/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // this code uses a selector that selects the h1 elements and changes 
+    // the background color to red
+    
+    // $('h1').click(function(){
+    //     $(this).css('background-color', '#ff0000')
+    // })
+
+});
